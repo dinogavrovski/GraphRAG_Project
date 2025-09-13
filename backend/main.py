3e81316e21a7914465f43087f53b5b6022243c7a
Dino Gavrovski <133152477+dinogavrovski@users.noreply.github.com>
2025-09-13 22:28:05 +0200

    Create Backend JWT Token Authentication

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Depends, HTTPException
+from backend import models
 from dotenv import load_dotenv
 from backend.relaxation import run_nl_search
+from sqlalchemy.orm import Session
+from typing import Annotated
+from backend.userdb import engine, SessionLocal
+from backend import auth
+from backend.auth import get_current_user
 
 load_dotenv()
 app = FastAPI(title="Car GraphRAG API")
 
+app.include_router(auth.router)
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def user(user: user_dependency, db:db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication failed")
+    return {"User": user}
 
 @app.post("/search")
 def search(query: str):
@@ -17,8 +44,3 @@ def search(query: str):
     """
     results, cypher_query = run_nl_search(query)
     return {"query": cypher_query, "results": results}
-
-
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
